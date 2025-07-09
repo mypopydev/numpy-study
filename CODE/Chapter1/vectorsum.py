3a0614475daed3c20aeb43d03a736ba868e9e9dd
Jun Zhao <barryjzhao@tencent.com>
2025-07-09 23:44:38 +0800

    Refactor vectorsum.py for Python 3 compatibility
    - Update shebang to use python3
    - Remove obsolete __future__ import
    - Change range objects to lists for index assignment
    - Use list comprehensions in pythonsum for clarity and Python 3 compatibility
    - Improve elapsed time calculation using total_seconds()
    - Add argument check and usage message for better error handling
    - Minor formatting and whitespace cleanup
    
    Signed-off-by: Jun Zhao <barryjzhao@tencent.com>

--- CODE/Chapter1/vectorsum.py
+++ CODE/Chapter1/vectorsum.py
@@ -1,54 +1,49 @@
-#!/usr/bin/env/python
+#!/usr/bin/env python3
 
-from __future__ import print_function
-import sys
 from datetime import datetime
+import sys
 import numpy as np
 
 """
  Chapter 1 of NumPy Beginners Guide.
  This program demonstrates vector addition the Python way.
  Run from the command line as follows
-     
+
   python vectorsum.py n
- 
+
  where n is an integer that specifies the size of the vectors.
 
- The first vector to be added contains the squares of 0 up to n. 
+ The first vector to be added contains the squares of 0 up to n.
  The second vector contains the cubes of 0 up to n.
  The program prints the last 2 elements of the sum and the elapsed time.
 """
 
 def numpysum(n):
-   a = np.arange(n) ** 2
-   b = np.arange(n) ** 3
-   c = a + b
-
-   return c
+    a = np.arange(n) ** 2
+    b = np.arange(n) ** 3
+    c = a + b
+    return c
 
 def pythonsum(n):
-   a = range(n)
-   b = range(n)
-   c = []
+    a = [i ** 2 for i in range(n)]
+    b = [i ** 3 for i in range(n)]
+    c = [a[i] + b[i] for i in range(n)]
+    return c
 
-   for i in range(len(a)):
-       a[i] = i ** 2
-       b[i] = i ** 3
-       c.append(a[i] + b[i])
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python vectorsum.py n")
+        sys.exit(1)
+    size = int(sys.argv[1])
 
-   return c
-   
+    start = datetime.now()
+    c = pythonsum(size)
+    delta = datetime.now() - start
+    print("The last 2 elements of the sum", c[-2:])
+    print("PythonSum elapsed time in microseconds", delta.total_seconds() * 1e6)
 
-size = int(sys.argv[1])
-
-start = datetime.now()
-c = pythonsum(size)
-delta = datetime.now() - start
-print("The last 2 elements of the sum", c[-2:])
-print("PythonSum elapsed time in microseconds", delta.microseconds)
-
-start = datetime.now()
-c = numpysum(size)
-delta = datetime.now() - start
-print("The last 2 elements of the sum", c[-2:])
-print("NumPySum elapsed time in microseconds", delta.microseconds)
+    start = datetime.now()
+    c = numpysum(size)
+    delta = datetime.now() - start
+    print("The last 2 elements of the sum", c[-2:])
+    print("NumPySum elapsed time in microseconds", delta.total_seconds() * 1e6)
